b8a3c3029d0ada0e2138f8e2636038354dbf3fc2
limoragam <33860860+limoragam@users.noreply.github.com>
2018-06-06 23:33:55 +0300

    Share buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fadeInRight', [
+      transition(':enter', [
+        style({opacity:0, transform:'translateX(-15px)'}),
+        animate("300ms 100ms", style({opacity:1, transform:'translateX(0)'})) 
+      ]),
+      transition(':leave', [
+        animate(500, style({opacity:0, transform:'translateX(-15px)'})) 
+      ])
+    ]),
+    trigger('fadeInLeft', [
+      transition(':enter', [
+        style({opacity:0, transform:'translateX(15px)'}),
+        animate("300ms 100ms", style({opacity:1, transform:'translateX(0)'})) 
+      ]),
+      transition(':leave', [
+        animate(500, style({opacity:0, transform:'translateX(15px)'})) 
+      ])
+    ]),
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  showSocial = false;
+  isPhone = true;
+
+  constructor() {
+    if(window.innerWidth >= 600) {
+      this.isPhone = false;
+    }
+  }
+  
+  showSocialIcons(event:Event) {
+    this.showSocial = this.showSocial ? false : true;
+    return false;
+  }
 }
